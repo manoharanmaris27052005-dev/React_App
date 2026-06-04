4b79facf161a213756aa0f0490141ebaa69739a1
Dinesh107 <dineshjr107@gmail.com>
2026-06-04 19:15:08 +0530

    2nd commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
 import EmployeeForm from "./components/EmployeeForm.jsx";
 import FavoriteCar from "./components/FavoriteCar.jsx";
 import Header from "./components/Header";
+import Home from "./components/Home.jsx";
 import List from "./components/List.jsx";
 import Office from "./components/Office";
 import Patient from "./components/Patient.jsx";
 import StopWatch from "./components/StopWatch.jsx";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 
 function App() {
@@ -18,7 +22,26 @@ function App() {
         {/* <Patient/> */}
         {/* <List/> */}
         {/* <StopWatch/> */}
-        <EmployeeForm/>
+        {/* <EmployeeForm/> */}
+
+        {/* <Home/>
+        <About/>
+        <Contact/> */}
+
+        
+        <BrowserRouter>
+        <ul>
+          <li><Link to="/">Home Page</Link></li>
+          <li><Link to="/about">About Page</Link></li>
+          <li><Link to="/contact-us">Contact page</Link></li>
+        </ul>
+            <Routes>
+                <Route path='/' element={ <Home/> } />
+                <Route path='/about' element={ <About/> } />
+                <Route path='/contact-us' element={ <Contact/> } />
+            </Routes>
+        </BrowserRouter>
+
     </>
   );
 }
